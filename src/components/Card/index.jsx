84709816a6191f6c55ac855151baae007cca5d11
Thiago de Mattos Santos <wwthiagothims@gmail.com>
2023-08-14 21:10:12 -0300

    feat: footer with sponsor test

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss'
+
+function Card() {
+  return(
+    <>
+    </>
+  )
+}
+
+export default Card
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,15 +2,15 @@ import styles from './styles.module.scss'
 import Slider from '../../components/Slider'
 import Button from '../../components/Button'
 import Article from '../../components/Article'
+import Footer from '../../components/Footer'
 
 function Home() {
   return(
     <section className={styles.home_container}>
       <Slider />
       <Button classButton={'mainButton'} link='/signup' type='text' text='Inscreva-se já' />
-      <Article
-
-      />
+      <Article />
+      <Footer />
     </section>
   )
 }
